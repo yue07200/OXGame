b04c50c5d5b907895ff1efbe2c972bc52e9f83a8
s1120369@o365st.pu.edu.tw <s1120369@o365st.pu.edu.tw>
2024-03-26 01:18:30 +0800

    第一次提交

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace OXGame 
+﻿namespace OXGame
 {
     partial class Form1
     {
@@ -28,135 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 120);
-            this.button1.TabIndex = 1;
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(34, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(124, 120);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(166, 30);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(124, 120);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = true;
+            button2.Location = new Point(166, 30);
+            button2.Name = "button2";
+            button2.Size = new Size(124, 120);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(296, 29);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(124, 120);
-            this.button3.TabIndex = 3;
-            this.button3.UseVisualStyleBackColor = true;
+            button3.Location = new Point(296, 29);
+            button3.Name = "button3";
+            button3.Size = new Size(124, 120);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(34, 156);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(124, 120);
-            this.button4.TabIndex = 4;
-            this.button4.UseVisualStyleBackColor = true;
+            button4.Location = new Point(34, 156);
+            button4.Name = "button4";
+            button4.Size = new Size(124, 120);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(166, 156);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(124, 120);
-            this.button5.TabIndex = 5;
-            this.button5.UseVisualStyleBackColor = true;
+            button5.Location = new Point(166, 156);
+            button5.Name = "button5";
+            button5.Size = new Size(124, 120);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(294, 155);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(124, 120);
-            this.button6.TabIndex = 6;
-            this.button6.UseVisualStyleBackColor = true;
+            button6.Location = new Point(294, 155);
+            button6.Name = "button6";
+            button6.Size = new Size(124, 120);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button1_Click;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(34, 282);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(124, 120);
-            this.button7.TabIndex = 7;
-            this.button7.UseVisualStyleBackColor = true;
+            button7.Location = new Point(34, 282);
+            button7.Name = "button7";
+            button7.Size = new Size(124, 120);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button1_Click;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(166, 282);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(124, 120);
-            this.button8.TabIndex = 8;
-            this.button8.UseVisualStyleBackColor = true;
+            button8.Location = new Point(166, 282);
+            button8.Name = "button8";
+            button8.Size = new Size(124, 120);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button1_Click;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(294, 282);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(124, 120);
-            this.button9.TabIndex = 9;
-            this.button9.UseVisualStyleBackColor = true;
+            button9.Location = new Point(294, 282);
+            button9.Name = "button9";
+            button9.Size = new Size(124, 120);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button1_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(477, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 50);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Player";
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(477, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 50);
+            label1.TabIndex = 10;
+            label1.Text = "Player";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 64F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label2.Location = new System.Drawing.Point(477, 94);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 135);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "X";
+            label2.AutoSize = true;
+            label2.Cursor = Cursors.IBeam;
+            label2.Font = new Font("Microsoft JhengHei UI", 64F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.MenuHighlight;
+            label2.Location = new Point(477, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 135);
+            label2.TabIndex = 11;
+            label2.Text = "X";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 429);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Tic-Tac-Toe Game, by S1125566";
-            this.Load += new System.EventHandler(this.Form1_Load_1);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(662, 429);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Tic-Tac-Toe Game, by S1125566";
+            Load += Form1_Load_1;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,11 @@
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
-namespace OXGame 
+namespace OXGame
 {
     public partial class Form1 : Form
     {
+        string playerMarker = "X";
         public Form1()
         {
             InitializeComponent();
@@ -28,5 +30,70 @@ namespace OXGame
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Text = playerMarker;
+            checkWinner();
+            playerMarker = playerMarker == "X" ? "O" : "X"; //如果是 "X"，則返回 "O"，否則返回 "X"。
+            label2.Text = playerMarker;
+
+
+        }
+        private void checkWinner()
+        {
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text == playerMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+                //return ; // 玩家獲勝後立即返回，不再繼續檢查其他可能的獲勝組合hj/
+
+            }
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text == playerMarker)
+            {
+                MessageBox.Show(button4.Text, "Winner is...");
+
+            }
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text == playerMarker)
+            {
+                MessageBox.Show(button7.Text, "Winner is...");
+
+            }
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text == playerMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+
+            }
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text == playerMarker)
+            {
+                MessageBox.Show(button2.Text, "Winner is...");
+
+            }
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text == playerMarker)
+            {
+                MessageBox.Show(button3.Text, "Winner is...");
+
+            }
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text == playerMarker)
+            {
+                MessageBox.Show(button1.Text, "Winner is...");
+
+            }
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text == playerMarker)
+            {
+                MessageBox.Show(button3.Text, "Winner is...");
+
+            }
+            //DialogResult result = MessageBox.Show(MessageBoxButtons.YesNo);
+
+            //if (result == DialogResult.Yes)
+            //{
+            //}
+            //else if (result == DialogResult.No)
+            //{
+            //}
+        }
+
+
     }
 }
